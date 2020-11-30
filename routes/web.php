3ce2301b5e498fb19160ede3e79b663775962a63
Alec Madeira <madeiraalec@gmail.com>
2020-11-30 23:06:31 +0000

    Added User Account relationship to User model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Models\AccountUser;
+use App\Models\AccountUserRole;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Models\Account;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +20,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+
+    /* $accountUser = new AccountUser();
+     $accountUser->account_id = 1;
+     $accountUser->user_id = 1;
+     $accountUser->status = AccountUser::STATUS_ACTIVE;
+     $accountUser->save();*/
+
+    /*    $accountUser = AccountUser::find(1);
+        var_dump($accountUser->account->createdBy);*/
+
+//
+//    $user = User::find(1);
+//    var_dump($user->accounts[0]->roles[0]->accountUser->account->createdBy);
+
 });
